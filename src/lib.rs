5bd140dc10e80410f5f1f189a6585287dca720f8
Cathie <cathie@chain.com>
2018-02-05 16:23:07 -0800

    Output range proof

--- src/lib.rs
+++ src/lib.rs
@@ -14,13 +14,13 @@ use sha2::{Digest, Sha256, Sha512};
 use curve25519_dalek::scalar::Scalar;
 use rand::OsRng;
 
-struct Degree3Poly {
-    pub d0: Scalar,
-    pub d1: Scalar,
-    pub d2: Scalar,
-}
+struct Degree3Poly(Scalar, Scalar, Scalar);
 
-struct RangeProof {}
+struct RangeProof {
+    tau: Scalar,
+    mu: Scalar,
+    t: Scalar,
+}
 
 impl RangeProof {
     pub fn generate_proof(
@@ -36,8 +36,8 @@ impl RangeProof {
         let a_vec = make_generators(a, len);
 
         // Compute big_a (in the paper: A; line 36-39)
-        let alpha = RistrettoPoint::random(&mut rng);
-        let mut big_a = alpha.clone();
+        let alpha = Scalar::random(&mut rng);
+        let mut big_a = b * alpha;
         for i in 0..len {
             let v_i = (v >> i) & 1;
             if v_i == 0 {
@@ -53,42 +53,55 @@ impl RangeProof {
         let big_s = ristretto::multiscalar_mult(&randomness, points_iter);
 
         // Save/label randomness (rho, s_L, s_R) to be used later
-        let _rho = &randomness[0];
+        let rho = &randomness[0];
         let s_l = &randomness[1..len + 1];
         let s_r = &randomness[len + 1..2 * len + 1];
 
         // Generate y, z by committing to A, S (line 43-45)
         let (y, z) = commit(&big_a, &big_s);
 
-        // Calculate t1, t2 (line 46)
+        // Calculate t (line 46)
         let mut t = Degree3Poly::new();
         let mut v_temp = v.clone();
         let mut exp_y = Scalar::one(); // start at y^0 = 1
         let mut exp_2 = Scalar::one(); // start at 2^0 = 1
         let z2 = z * z;
+        let z3 = z2 * z;
 
         for i in 0..len {
-            t.d1 += s_l[i] * exp_y * z + s_l[i] * z2 * exp_2 + s_r[i] * exp_y * (-z);
-            t.d2 += s_l[i] * exp_y * s_r[i];
+            t.0 += exp_y * (z - z2) - z3 * exp_2;
+            t.1 += s_l[i] * exp_y * z + s_l[i] * z2 * exp_2 + s_r[i] * exp_y * (-z);
+            t.2 += s_l[i] * exp_y * s_r[i];
             // check if a_l is 0 or 1
             if v_temp & 1 == 0 {
-                t.d1 -= s_l[i] * exp_y;
+                t.1 -= s_l[i] * exp_y;
             } else {
-                t.d1 += s_r[i] * exp_y;
+                t.0 += z2 * exp_2;
+                t.1 += s_r[i] * exp_y;
             }
             v_temp = v_temp >> 1; // bit-shift v by one
             exp_y = exp_y * y; // y^i -> y^(i+1)
             exp_2 = exp_2 + exp_2; // 2^i -> 2^(i+1)
         }
 
-        // Generate x by committing to T_1, T_2 (line 47-51)
+        // Generate x by committing to big_t_1, big_t_2 (in the paper: T1, T2; line 47-51)
         let tau_1 = Scalar::random(&mut rng);
         let tau_2 = Scalar::random(&mut rng);
-        let big_t_1 = b * t.d1 + a * tau_1;
-        let big_t_2 = b * t.d2 + a * tau_2;
-        let (x, _) = commit(&big_t_1, &big_t_2);
+        let big_t_1 = b * t.1 + a * tau_1;
+        let big_t_2 = b * t.2 + a * tau_2;
+        let (x, _) = commit(&big_t_1, &big_t_2); // TODO: use a different commit
 
-        unimplemented!()
+        // Generate final values for proof (line 52-54)
+        let gamma = Scalar::random(&mut rng);
+        let tau_x = tau_1 * x + tau_2 * x * x + z2 * gamma;
+        let mu = alpha + rho * x;
+        let t_total = t.0 + t.1 * x + t.2 * x * x;
+
+        RangeProof {
+            tau: tau_x,
+            mu: mu,
+            t: t_total,
+        }
     }
 
     pub fn verify_proof() -> Result<(), ()> {
@@ -97,12 +110,8 @@ impl RangeProof {
 }
 
 impl Degree3Poly {
-    pub fn new() -> Self {
-        Self {
-            d0: Scalar::zero(),
-            d1: Scalar::zero(),
-            d2: Scalar::zero(),
-        }
+    pub fn new() -> Degree3Poly {
+        Degree3Poly(Scalar::zero(), Scalar::zero(), Scalar::zero())
     }
 }
 
