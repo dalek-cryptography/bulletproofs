7f2bc6d35dbdec5b57f6e428d0f1fc959a61a037
Cathie <cathie@chain.com>
2018-03-02 14:42:26 -0800

    restructure to make commitments, rename parts

--- src/multi_range_proof.rs
+++ src/multi_range_proof.rs
@@ -1,7 +1,7 @@
 #![allow(non_snake_case)]
 
 use std::iter;
-use sha2::{Digest, Sha256, Sha512};
+use sha2::Sha256;
 use curve25519_dalek::ristretto::RistrettoPoint;
 use curve25519_dalek::ristretto;
 use curve25519_dalek::traits::Identity;
@@ -21,31 +21,31 @@ pub struct Generators {
     n: usize,
 }
 
-pub struct State1 {
-    // committed values (public)
+pub struct InputCommitment {
     pub V: RistrettoPoint,
     pub A: RistrettoPoint,
-    pub S: RistrettoPoint,
+    pub S: RistrettoPoint,    
+}
 
-    // intermediate values (private)
+pub struct Input {
+    pub gen: Generators,
+    pub inp_comm: InputCommitment,
+
     v_blinding: Scalar,
     a_blinding: Scalar,
     randomness: Vec<Scalar>,
-    v: u64,
-
-    // public knowledge
-    n: usize,
-    B: RistrettoPoint,
-    B_blinding: RistrettoPoint,   
+    v: u64,  
 }
 
-pub struct State2 {
-    // committed values (public)
-    V: RistrettoPoint, // TBD: do we still need to save V, A, S? The've been committed/sent already...
-    A: RistrettoPoint,
-    S: RistrettoPoint,
+pub struct StatementCommitment {
     pub T_1: RistrettoPoint,
-    pub T_2: RistrettoPoint,
+    pub T_2: RistrettoPoint, 
+}
+
+pub struct Statement {
+    pub gen: Generators,
+    pub inp_comm: InputCommitment,
+    pub st_comm: StatementCommitment,
 
     // intermediate values (private)
     y: Scalar,
@@ -58,37 +58,25 @@ pub struct State2 {
     s_blinding: Scalar,
     t_1_blinding: Scalar,
     t_2_blinding: Scalar,
-
-    // public knowledge
-    n: usize,
-    B: RistrettoPoint,
-    B_blinding: RistrettoPoint,     
 }
 
-pub struct State3 {
+pub struct Proof {
+    pub gen: Generators,
+    pub inp_comm: InputCommitment,
+    pub st_comm: StatementCommitment,
+
     t_x_blinding: Scalar,
     e_blinding: Scalar,
     t: Scalar,
+
     // don't need if doing inner product proof
     l: Vec<Scalar>,
     r: Vec<Scalar>,
-
-    // committed values
-    V: RistrettoPoint,
-    A: RistrettoPoint,
-    S: RistrettoPoint,
-    T_1: RistrettoPoint,
-    T_2: RistrettoPoint,
-
-    // public knowledge
-    n: usize,
-    B: RistrettoPoint,
-    B_blinding: RistrettoPoint,
 }
 
 
 impl Generators {
-    pub fn make(n: usize) -> Generators {
+    pub fn new(n: usize) -> Generators {
         let B = RistrettoPoint::hash_from_bytes::<Sha256>("hello".as_bytes());
         let B_blinding = RistrettoPoint::hash_from_bytes::<Sha256>("there".as_bytes());
         Generators {
@@ -100,7 +88,7 @@ impl Generators {
         }
     }
 
-    pub fn prove(&self, v: u64) -> State1 {
+    pub fn make_input(&self, v: u64) -> Input {
         let mut rng: OsRng = OsRng::new().unwrap();
         // useful for debugging:
         // let mut rng: StdRng = StdRng::from_seed(&[1, 2, 3, 4]); 
@@ -125,43 +113,43 @@ impl Generators {
         let points_iter = iter::once(&self.B_blinding).chain((&self.G).iter()).chain((&self.H).iter());
         let randomness: Vec<_> = (0..(1 + 2 * self.n)).map(|_| Scalar::random(&mut rng)).collect();
         let S = ristretto::multiscalar_mult(&randomness, points_iter);
-
-        State1 {
+        let inp_comm = InputCommitment {
             V: V,
             A: A,
-            S: S,
+            S: S,            
+        };
 
+        Input {
+            gen: *self,
+            inp_comm: inp_comm,
             v_blinding: v_blinding,
             a_blinding: a_blinding,
             randomness: randomness,
             v: v,
-
-            n: self.n,
-            B: self.B,
-            B_blinding: self.B_blinding,
         }
     }
 }
 
-impl State1 {
-    pub fn prove(&self, y: Scalar, z: Scalar) -> State2 {
+impl Input {
+    pub fn make_statement(&self, y: Scalar, z: Scalar) -> Statement {
         let mut rng: OsRng = OsRng::new().unwrap();
         // useful for debugging:
         // let mut rng: StdRng = StdRng::from_seed(&[1, 2, 3, 4]);
+        let n = self.gen.n;
 
         // Save/label randomness to be used later (in the paper: rho, s_L, s_R)
         let s_blinding = self.randomness[0];
-        let s_a = &self.randomness[1..(self.n + 1)];
-        let s_b = &self.randomness[(self.n + 1)..(1 + 2 * self.n)];
+        let s_a = &self.randomness[1..(n + 1)];
+        let s_b = &self.randomness[(n + 1)..(1 + 2 * n)];
 
         // Calculate t by calculating vectors l0, l1, r0, r1 and multiplying
-        let mut l = VecPoly2::new(self.n);
-        let mut r = VecPoly2::new(self.n);
+        let mut l = VecPoly2::new(n);
+        let mut r = VecPoly2::new(n);
         let z2 = z * z;
         let mut t = PolyDeg3::new();
         let mut exp_y = Scalar::one(); // start at y^0 = 1
         let mut exp_2 = Scalar::one(); // start at 2^0 = 1
-        for i in 0..self.n {
+        for i in 0..n {
             let v_i = (self.v >> i) & 1;
             l.0[i] -= z;
             l.1[i] += s_a[i];
@@ -186,15 +174,17 @@ impl State1 {
         // Generate x by committing to T_1, T_2 (line 49-54)
         let t_1_blinding = Scalar::random(&mut rng);
         let t_2_blinding = Scalar::random(&mut rng);
-        let T_1 = self.B * t.1 + self.B_blinding * t_1_blinding;
-        let T_2 = self.B * t.2 + self.B_blinding * t_2_blinding;
-
-        State2 {
-            V: self.V,
-            A: self.A,
-            S: self.S,
+        let T_1 = self.gen.B * t.1 + self.gen.B_blinding * t_1_blinding;
+        let T_2 = self.gen.B * t.2 + self.gen.B_blinding * t_2_blinding;
+        let st_comm = StatementCommitment {
             T_1: T_1,
-            T_2: T_2,
+            T_2: T_2,            
+        };
+
+        Statement {
+            gen: self.gen,
+            inp_comm: self.inp_comm,
+            st_comm: st_comm,
             y: y,
             z: z,
             l: l,
@@ -205,15 +195,12 @@ impl State1 {
             s_blinding: s_blinding,
             t_1_blinding: t_1_blinding,
             t_2_blinding: t_2_blinding,
-            n: self.n,
-            B: self.B,
-            B_blinding: self.B_blinding,
         }
     }
 }
 
-impl State2 {
-    pub fn prove(&self, x: Scalar) -> State3 {
+impl Statement {
+    pub fn make_proof(&self, x: Scalar) -> Proof {
         // Generate final values for proof (line 55-60)
         let t_x_blinding = self.t_1_blinding * x + self.t_2_blinding * x * x + self.z * self.z * self.v_blinding;
         let e_blinding = self.a_blinding + self.s_blinding * x;
@@ -224,40 +211,48 @@ impl State2 {
         let l_total = self.l.eval(x);
         let r_total = self.r.eval(x);
 
-        State3 {
+        Proof {
+            gen: self.gen,
+            inp_comm: self.inp_comm,
+            st_comm: self.st_comm,
             t_x_blinding: t_x_blinding,
             e_blinding: e_blinding,
             t: t_hat,
             l: l_total,
             r: r_total,
-
-            V: self.V,
-            A: self.A,
-            S: self.S,
-            T_1: self.T_1,
-            T_2: self.T_2,
-
-            n: self.n,
-            B: self.B,
-            B_blinding: self.B_blinding,
         }
     }
 }
 
-impl State3 {
-    pub fn prove_single(v: u64, n: usize) -> State3 {
-        let state1 = Generators::make(n).prove(v);
-        let (y, z) = commit(&state1.A, &state1.S);
-        let state2 = state1.prove(y, z);
-        let (x, _) = commit(&state2.T_1, &state2.T_2);
-        state2.prove(x)
+impl Proof {
+    pub fn prove_single(v: u64, n: usize) -> Proof {
+        let input = Generators::new(n).make_input(v);
+        // TODO: swap out this commitment with use of RO
+        let (y, z) = commit(&input.inp_comm.A, &input.inp_comm.S);
+        let statement = input.make_statement(y, z);
+        // TODO: swap out this commitment with use of RO
+        let (x, _) = commit(&statement.st_comm.T_1, &statement.st_comm.T_2);
+        statement.make_proof(x)
+    }
+
+    pub fn combine(proofs: Vec<Proof>) -> Proof {
+        unimplemented!()
     }
 
     pub fn verify_proof(&self) -> bool {
-        let (y, z) = commit(&self.A, &self.S);
-        let (x, _) = commit(&self.T_1, &self.T_2);
-        let G = make_generators(&self.B, self.n);
-        let mut hprime_vec = make_generators(&self.B_blinding, self.n);
+        let V = &self.inp_comm.V;
+        let A = &self.inp_comm.A;
+        let S = &self.inp_comm.S;
+        let T_1 = &self.st_comm.T_1;
+        let T_2 = &self.st_comm.T_2;
+        let n = self.gen.n;
+        let B = &self.gen.B;
+        let B_blinding = &self.gen.B_blinding;
+
+        let (y, z) = commit(A, S);
+        let (x, _) = commit(T_1, T_2);
+        let G = make_generators(B, n);
+        let mut hprime_vec = make_generators(B_blinding, n);
 
         // line 63: check that t = t0 + t1 * x + t2 * x * x
         let z2 = z * z;
@@ -265,14 +260,14 @@ impl State3 {
         let mut power_g = Scalar::zero();
         let mut exp_y = Scalar::one(); // start at y^0 = 1
         let mut exp_2 = Scalar::one(); // start at 2^0 = 1
-        for _ in 0..self.n {
+        for _ in 0..n {
             power_g += (z - z2) * exp_y - z3 * exp_2;
 
             exp_y = exp_y * y; // y^i -> y^(i+1)
             exp_2 = exp_2 + exp_2; // 2^i -> 2^(i+1)
         }
-        let t_check = self.B * power_g + self.V * z2 + self.T_1 * x + self.T_2 * x * x;
-        let t_commit = self.B * self.t + self.B_blinding * self.t_x_blinding;
+        let t_check = B * power_g + V * z2 + T_1 * x + T_2 * x * x;
+        let t_commit = B * self.t + B_blinding * self.t_x_blinding;
         if t_commit != t_check {
             //println!("fails check on line 63");
             return false;
@@ -281,17 +276,17 @@ impl State3 {
         // line 62: calculate hprime
         // line 64: compute commitment to l, r
         let mut sum_G = RistrettoPoint::identity();
-        for i in 0..self.n {
+        for i in 0..n {
             sum_G += G[i];
         }
-        let mut big_p = self.A + self.S * x;
+        let mut big_p = A + S * x;
         big_p -= sum_G * z;
 
         let mut exp_y = Scalar::one(); // start at y^0 = 1
         let mut exp_2 = Scalar::one(); // start at 2^0 = 1
         let inverse_y = Scalar::invert(&y);
         let mut inv_exp_y = Scalar::one(); // start at y^-0 = 1
-        for i in 0..self.n {
+        for i in 0..n {
             hprime_vec[i] = hprime_vec[i] * inv_exp_y;
             big_p += hprime_vec[i] * (z * exp_y + z2 * exp_2);
             exp_y = exp_y * y; // y^i -> y^(i+1)
@@ -300,7 +295,7 @@ impl State3 {
         }
 
         // line 65: check that l, r are correct
-        let mut big_p_check = self.B_blinding * self.e_blinding;
+        let mut big_p_check = B_blinding * self.e_blinding;
         let points_iter = G.iter().chain(hprime_vec.iter());
         let scalars_iter = self.l.iter().chain(self.r.iter());
         big_p_check += ristretto::multiscalar_mult(scalars_iter, points_iter);
@@ -345,7 +340,7 @@ mod tests {
     use rand::Rng;
 
     #[test]
-    fn verify_smrp_simple() {
+    fn verify_multirp_simple() {
         for n in &[1, 2, 4, 8, 16, 32] {
             //println!("n: {:?}", n);
             let rp = State3::prove_single(0, *n);
@@ -361,7 +356,7 @@ mod tests {
         }
     }
     #[test]
-    fn verify_smrp_rand_big() {
+    fn verify_multirp_rand_big() {
         for _ in 0..50 {
             let mut rng: OsRng = OsRng::new().unwrap();
             let v: u64 = rng.next_u64();
@@ -371,7 +366,7 @@ mod tests {
         }
     }
     #[test]
-    fn verify_smrp_rand_small() {
+    fn verify_multirp_rand_small() {
         for _ in 0..50 {
             let mut rng: OsRng = OsRng::new().unwrap();
             let v: u32 = rng.next_u32();
@@ -388,23 +383,23 @@ mod bench {
     use test::Bencher;
 
     #[bench]
-    fn make_smrp_64(b: &mut Bencher) {
+    fn make_multirp_64(b: &mut Bencher) {
         let mut rng: OsRng = OsRng::new().unwrap();
         b.iter(|| State3::prove_single(rng.next_u64(), 64));
     }
     #[bench]
-    fn make_smrp_32(b: &mut Bencher) {
+    fn make_multirp_32(b: &mut Bencher) {
         let mut rng: OsRng = OsRng::new().unwrap();
         b.iter(|| State3::prove_single(rng.next_u32() as u64, 32));
     }
     #[bench]
-    fn verify_smrp_64(b: &mut Bencher) {
+    fn verify_multirp_64(b: &mut Bencher) {
         let mut rng: OsRng = OsRng::new().unwrap();
         let rp = State3::prove_single(rng.next_u64(), 64);
         b.iter(|| rp.verify_proof());
     }
     #[bench]
-    fn verify_smrp_32(b: &mut Bencher) {
+    fn verify_multirp_32(b: &mut Bencher) {
         let mut rng: OsRng = OsRng::new().unwrap();
         let rp = State3::prove_single(rng.next_u32() as u64, 32);
         b.iter(|| rp.verify_proof());
