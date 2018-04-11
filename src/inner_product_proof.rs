8117a1cdf677c166e599e87fb2e18e8df3b2cc43
Cathie <cathie@chain.com>
2018-04-11 11:28:45 -0700

    use for_each directly

--- src/inner_product_proof.rs
+++ src/inner_product_proof.rs
@@ -109,16 +109,16 @@ impl Proof {
             // Parallelized calculation of each index of the vectors G and H.
             // G_L[i] = G_L[i] * x_inv + G_R[i] * x (for all i)
             G_L.par_iter_mut().zip(G_R.par_iter())
-                .map(|(G_L_i, G_R_i)| {
+                .for_each(|(G_L_i, G_R_i)| {
                     *G_L_i = ristretto::multiscalar_mul(&[x_inv, x], &[*G_L_i, *G_R_i]);
                     }
-                ).for_each(|()|{});
+                );
             // H_L[i] = H_L[i] * x + H_R[i] * x_inv (for all i)
             H_L.par_iter_mut().zip(H_R.par_iter())
-                .map(|(H_L_i, H_R_i)| {
+                .for_each(|(H_L_i, H_R_i)| {
                     *H_L_i = ristretto::multiscalar_mul(&[x, x_inv], &[*H_L_i, *H_R_i]);
                     }
-                ).for_each(|()|{});
+                );
 
             a = a_L;
             b = b_L;
