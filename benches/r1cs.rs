002498ea50d703e6f432fb0129c1e1d047cead57
Cathie Yun <cathieyun@gmail.com>
2018-10-09 11:40:42 -0700

    add kshuffle specs

--- benches/r1cs.rs
+++ benches/r1cs.rs
@@ -17,6 +17,56 @@ use merlin::Transcript;
 extern crate rand;
 use rand::Rng;
 
+/* 
+K-SHUFFLE GADGET SPECIFICATION:
+
+Represents a permutation of a list of `k` scalars `{x_i}` into a list of `k` scalars `{y_i}`.
+
+Algebraically it can be expressed as a statement that for a free variable `z`, 
+the roots of the two polynomials in terms of `z` are the same up to a permutation:
+
+    ∏(x_i - z) == ∏(y_i - z)
+
+Prover can commit to blinded scalars `x_i` and `y_i` then receive a random challenge `z`, 
+and build a proof that the above relation holds.
+
+K-shuffle requires `2*(K-1)` multipliers.
+
+For K > 1:
+
+        (x_0 - z)---⊗------⊗---(y_0 - z)        // mulx[0], muly[0]
+                    |      |
+        (x_1 - z)---⊗      ⊗---(y_1 - z)        // mulx[1], muly[1]
+                    |      |
+                   ...    ...
+                    |      |
+    (x_{k-2} - z)---⊗      ⊗---(y_{k-2} - z)    // mulx[k-2], muly[k-2]
+                   /        \
+    (x_{k-1} - z)_/          \_(y_{k-1} - z)
+
+    // Connect left and right sides of the shuffle statement
+    mulx_out[0] = muly_out[0]
+
+    // For i == [0, k-3]:
+    mulx_left[i]  = x_i - z
+    mulx_right[i] = mulx_out[i+1]
+    muly_left[i]  = y_i - z
+    muly_right[i] = muly_out[i+1]
+
+    // last multipliers connect two last variables (on each side)
+    mulx_left[k-2]  = x_{k-2} - z
+    mulx_right[k-2] = x_{k-1} - z
+    muly_left[k-2]  = y_{k-2} - z
+    muly_right[k-2] = y_{k-1} - z
+
+For K = 1:
+
+    (x_0 - z)--------------(y_0 - z)
+
+    // Connect x to y directly, omitting the challenge entirely as it cancels out
+    x_0 = y_0
+*/
+
 // Make a gadget that adds constraints to a ConstraintSystem, such that the
 // y variables are constrained to be a valid shuffle of the x variables.
 struct KShuffleGadget {}
@@ -195,15 +245,16 @@ fn kshuffle_prove_helper(k: usize, c: &mut Criterion) {
     let label = format!("{}-shuffle proof creation", k);
 
     c.bench_function(&label, move |b| {
+        // Generate inputs and outputs to kshuffle
         let mut rng = rand::thread_rng();
         let (min, max) = (0u64, std::u64::MAX);
         let input: Vec<u64> = (0..k).map(|_| rng.gen_range(min, max)).collect();
         let mut output = input.clone();
         rand::thread_rng().shuffle(&mut output);
 
+        // Make kshuffle proof
         let pc_gens = PedersenGens::default();
         let bp_gens = BulletproofGens::new(128, 1);
-
         b.iter(|| {
             let mut prover_transcript = Transcript::new(b"ShuffleTest");
             let (prover_cs, _) =
@@ -272,21 +323,23 @@ fn kshuffle_verify_helper(k: usize, c: &mut Criterion) {
     let label = format!("{}-shuffle proof verification", k);
 
     c.bench_function(&label, move |b| {
+        // Generate inputs and outputs to kshuffle
         let mut rng = rand::thread_rng();
         let (min, max) = (0u64, std::u64::MAX);
         let input: Vec<u64> = (0..k).map(|_| rng.gen_range(min, max)).collect();
         let mut output = input.clone();
         rand::thread_rng().shuffle(&mut output);
 
+        // Make kshuffle proof
         let pc_gens = PedersenGens::default();
         let bp_gens = BulletproofGens::new(128, 1);
-
         let mut prover_transcript = Transcript::new(b"ShuffleTest");
         let (prover_cs, commitments) =
             kshuffle_prover_cs(&pc_gens, &bp_gens, &mut prover_transcript, &input, &output)
                 .unwrap();
         let proof = prover_cs.prove().unwrap();
 
+        // Verify kshuffle proof
         b.iter(|| {
             let mut verifier_transcript = Transcript::new(b"ShuffleTest");
             let verifier_cs =
