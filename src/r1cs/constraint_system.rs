7d1bd0c5b8caa737c517c0b2bfcf89f6aca8c01f
Cathie Yun <cathieyun@gmail.com>
2018-11-14 11:47:18 -0800

    add documentation for constraintsystem trait

--- src/r1cs/constraint_system.rs
+++ src/r1cs/constraint_system.rs
@@ -16,7 +16,17 @@ use curve25519_dalek::scalar::Scalar;
 /// using the `ConstraintSystem` trait, so that the prover and
 /// verifier share the logic for specifying constraints.
 pub trait ConstraintSystem {
-    /// XXX
+    /// Constrain `left`, `right`, and `out` linear combinations such that:
+    /// `left` = `l_var` (by adding a constraint)
+    /// `right` = `r_var` (by adding a constraint)
+    /// `out` = `o_var` (by adding a constraint)
+    /// `l_var` * `r_var` = `o_var` (by allocating multiplier variables)
+    /// Where:
+    /// `l_var` is either `left.eval()` (prover) or `Missing` (verifier)
+    /// `r_var` is either `right.eval()` (prover) or `Missing` (verifier)
+    /// `o_var` is either `out.eval()` (prover) or `Missing` (verifier)
+    /// This is used when all three gates of a multiplication gate should be
+    /// constrained by linear constraints.
     fn add_constraint(
         &mut self,
         left: LinearCombination,
@@ -24,7 +34,16 @@ pub trait ConstraintSystem {
         out: LinearCombination,
     ) -> (Variable, Variable, Variable);
 
-    /// XXX
+    /// Constrain `left` and `right` linear combinations such that:
+    /// `left` = `l_var` (by adding a constraint)
+    /// `right` = `r_var` (by adding a constraint)
+    /// `l_var` * `r_var` = `o_var` (by allocating multiplier variables)
+    /// Where:
+    /// `l_var` is either `left.eval()` (prover) or `Missing` (verifier)
+    /// `r_var` is either `right.eval()` (prover) or `Missing` (verifier)
+    /// `o_var` is either `left.eval() * right.eval()` (prover) or `Missing` (verifier)
+    /// This is used when only the left and right inputs of a multiplication gate
+    /// should be constrained by linear constraints.
     fn add_intermediate_constraint(
         &mut self,
         left: LinearCombination,
