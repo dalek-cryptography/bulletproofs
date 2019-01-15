65a412c129043163e1ece69e53b14677e278cc1d
Cathie Yun <cathieyun@gmail.com>
2019-01-15 15:46:21 -0800

    add back 'static type

--- src/r1cs/constraint_system.rs
+++ src/r1cs/constraint_system.rs
@@ -77,7 +77,7 @@ pub trait ConstraintSystem {
     /// ```
     fn specify_randomized_constraints<F>(&mut self, callback: F) -> Result<(), R1CSError>
     where
-        F: Fn(&mut Self::RandomizedCS) -> Result<(), R1CSError>;
+        F: 'static + Fn(&mut Self::RandomizedCS) -> Result<(), R1CSError>;
 }
 
 /// Represents a constraint system in the second phase:
--- src/r1cs/verifier.rs
+++ src/r1cs/verifier.rs
@@ -104,7 +104,7 @@ impl<'a, 'b> ConstraintSystem for Verifier<'a, 'b> {
 
     fn specify_randomized_constraints<F>(&mut self, callback: F) -> Result<(), R1CSError>
     where
-        F: Fn(&mut Self::RandomizedCS) -> Result<(), R1CSError>,
+        F: 'static + Fn(&mut Self::RandomizedCS) -> Result<(), R1CSError>,
     {
         self.deferred_constraints.push(Box::new(callback));
         Ok(())
@@ -135,7 +135,7 @@ impl<'a, 'b> ConstraintSystem for RandomizingVerifier<'a, 'b> {
 
     fn specify_randomized_constraints<F>(&mut self, callback: F) -> Result<(), R1CSError>
     where
-        F: Fn(&mut Self::RandomizedCS) -> Result<(), R1CSError>,
+        F: 'static + Fn(&mut Self::RandomizedCS) -> Result<(), R1CSError>,
     {
         callback(self)
     }
