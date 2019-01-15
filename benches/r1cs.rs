4fde6f33597bd5b8766a10b0f72f57c21bbc57ac
Ruben De Smet <ruben.de.smet@rubdos.be>
2019-01-15 20:52:17 +0100

    Make  bindings mutable again

--- benches/r1cs.rs
+++ benches/r1cs.rs
@@ -144,7 +144,7 @@ impl KShuffleGadget {
             .map(|v| prover.commit(*v, Scalar::random(&mut blinding_rng)))
             .unzip();
 
-        let cs = prover.finalize_inputs();
+        let mut cs = prover.finalize_inputs();
 
         Self::fill_cs(&mut cs, &input_vars, &output_vars);
 
@@ -178,7 +178,7 @@ impl KShuffleGadget {
             .map(|commitment| verifier.commit(*commitment))
             .collect();
 
-        let cs = verifier.finalize_inputs();
+        let mut cs = verifier.finalize_inputs();
 
         Self::fill_cs(&mut cs, &input_vars, &output_vars);
 
