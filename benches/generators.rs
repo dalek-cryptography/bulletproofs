be13f156a109609caf35742444292847526b03c3
Ruben De Smet <ruben.de.smet@rubdos.be>
2019-02-08 20:46:47 +0100

    Add benchmarks for BulletproofGens and PedersenGens (#252)

---
+++ benches/generators.rs
@@ -0,0 +1,26 @@
+extern crate bulletproofs;
+use bulletproofs::{BulletproofGens, PedersenGens};
+
+#[macro_use]
+extern crate criterion;
+use criterion::Criterion;
+
+fn pc_gens(c: &mut Criterion) {
+    c.bench_function("PedersenGens::new", |b| b.iter(|| PedersenGens::default()));
+}
+
+fn bp_gens(c: &mut Criterion) {
+    c.bench_function_over_inputs(
+        "BulletproofGens::new",
+        |b, size| b.iter(|| BulletproofGens::new(*size, 1)),
+        (0..10).map(|i| 2 << i),
+    );
+}
+
+criterion_group! {
+    bp,
+    bp_gens,
+    pc_gens,
+}
+
+criterion_main!(bp);
