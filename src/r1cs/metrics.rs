fe2c8023fc899d9998fb4921caa8ed578febc538
Ruben De Smet <ruben.de.smet@rubdos.be>
2020-05-25 14:06:35 +0200

    Add documentation for the new Metrics struct
    Co-authored-by: Oleg Andreev <oleganza@gmail.com>

--- src/r1cs/metrics.rs
+++ src/r1cs/metrics.rs
@@ -3,8 +3,12 @@
 /// See [`ConstraintSystem::metrics`](::r1cs::ConstraintSystem::metrics).
 #[derive(Debug, Clone)]
 pub struct Metrics {
+    /// Number of multiplicative constraints in the constraint system.
     pub multipliers: usize,
+    /// Total number of linear constraints in the constraint system.
     pub constraints: usize,
+    /// Number of linear constraints added in pre-randomization phase.
     pub phase_one_constraints: usize,
+    /// Number of linear constraints added in the randomization phase.
     pub phase_two_constraints: usize,
 }
