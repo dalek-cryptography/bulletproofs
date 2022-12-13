5c38d1705bad9c69224f1535ba72f0315bdc8d26
Erika <13k.erika@gmail.com>
2022-12-13 11:14:27 +0100

    Make InnerProductProof struct public

--- src/lib.rs
+++ src/lib.rs
@@ -33,6 +33,7 @@ mod transcript;
 
 pub use crate::errors::ProofError;
 pub use crate::generators::{BulletproofGens, BulletproofGensShare, PedersenGens};
+pub use crate::inner_product_proof::InnerProductProof;
 pub use crate::linear_proof::LinearProof;
 pub use crate::range_proof::RangeProof;
 
